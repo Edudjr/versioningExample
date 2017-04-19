67c9e630b35d67f3397bb10f45a8704a9236cb1a
Eduardo Domene Junior <eduardo.djr@hotmail.com>
2017-04-19 17:11:57 -0300

    Initial commit

---
+++ player.js
@@ -0,0 +1,15 @@
+//version 0.1.0
+//build number 1
+//tag 0.1.0.1
+
+class Player {
+  //TODO: Add constructor()
+
+  //TODO: Add Play()
+
+  //TODO: Add Pause()
+
+  //TODO: Add Stop()
+
+  //TODO: Add add()
+}
